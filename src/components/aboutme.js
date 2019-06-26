6aa7f32008b2db2b5125eae944b11744e966eea6
Devan Sheets <devansheets4@gmail.com>
2019-06-25 23:51:10 -0400

    Trying to figure out what to do with the about page

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,201 +1,77 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
-import charlotte from '../assets/charlotte.png';
+import { Grid, Cell, Button, Tab, Tabs } from 'react-mdl';
+// import charlotte from '../assets/charlotte.png';
 
-import freecodecamp from '../assets/freecodecamp.png';
-import udemy from '../assets/udemy.png';
-import skillshare from '../assets/skillshare.jpg';
-import youtube from '../assets/youtube.png';
+// import freecodecamp from '../assets/freecodecamp.png';
+// import udemy from '../assets/udemy.png';
+// import skillshare from '../assets/skillshare.jpg';
+// import youtube from '../assets/youtube.png';
 
-import html5 from '../assets/html5.png';
-import css3 from '../assets/css3.png';
-import javascript from '../assets/javascript.png';
-import angular from '../assets/angular.png';
-import react from '../assets/react.png';
-import nodejs from '../assets/nodejs.png';
-import mongodb from '../assets/mongodb.png';
+// import html5 from '../assets/html5.png';
+// import css3 from '../assets/css3.png';
+// import javascript from '../assets/javascript.png';
+// import angular from '../assets/angular.png';
+// import react from '../assets/react.png';
+// import nodejs from '../assets/nodejs.png';
+// import mongodb from '../assets/mongodb.png';
 
-import github from '../assets/github.png';
-import bitbucket from '../assets/bitbucket.png';
-import gitkracken from '../assets/gitkracken.png';
-import vscode from '../assets/vscode.png';
+// import github from '../assets/github.png';
+// import bitbucket from '../assets/bitbucket.png';
+// import gitkracken from '../assets/gitkracken.png';
+// import vscode from '../assets/vscode.png';
+// import family from '../assets/family.JPG';
 
 class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <div>
+          <h1>AboutMe</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1> What I do </h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div>
+          <h1> Where i'm from </h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 3) {
+      return (
+        <div>
+          <h1> What my goals are </h1>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
-      <div>
+      <div className='category-tabs'>
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>About Me</Tab>
+          <Tab>What I do </Tab>
+          <Tab>Where i'm from</Tab>
+          <Tab>My Goals</Tab>
+        </Tabs>
+
         <Grid>
-          <Cell className='about-header' col={4}>
-            <div>
-              <img
-                src={charlotte}
-                alt='charlotte-logo'
-              />
-            </div>
-          </Cell>
-
-          <Cell
-            className='about-header-right'
-            style={{ margin: 'auto' }}
-            col={8}
-          >
-            <div>
-              <h2>Bachelors Degree in Computer Science</h2>
-            </div>
-          </Cell>
-          <Cell className='cont-education' col={8}>
-            <div>
-              <h2>Continuing Education Courses</h2>
-            </div>
-          </Cell>
-          <Cell col={1}>
-            <a
-              href='https://www.udemy.com/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <img
-                src={udemy}
-                alt='udemy'
-                className='cont-ed-logo'
-              />
-            </a>
-          </Cell>
-          <Cell col={1}>
-            <a
-              href='https://www.skillshare.com/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <img
-                src={skillshare}
-                alt='skillshare'
-                className='cont-ed-logo'
-              />
-            </a>
-          </Cell>
-          <Cell col={1}>
-            <a
-              href='https://www.youtube.com/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <img
-                src={youtube}
-                alt='youtube'
-                className='cont-ed-logo'
-              />
-            </a>
-          </Cell>
-          <Cell col={1}>
-            <a
-              href='https://www.freecodecamp.org/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <img
-                src={freecodecamp}
-                alt='free-code-camp'
-                className='cont-ed-logo'
-              />
-            </a>
-          </Cell>
-
-          <Cell col={1}>
-            <img
-              src={html5}
-              alt='html5'
-              className='cont-ed-logo'
-            />
-          </Cell>
-
-          <Cell col={1}>
-            <img
-              src={css3}
-              alt='css3'
-              className='cont-ed-logo'
-            />
-          </Cell>
-
-          <Cell col={1}>
-            <img
-              src={javascript}
-              alt='javascript'
-              className='cont-ed-logo'
-            />
-          </Cell>
-
-          <Cell col={1}>
-            <img
-              src={angular}
-              alt='angular'
-              className='cont-ed-logo'
-            />
-          </Cell>
-
-          <Cell col={1}>
-            <img
-              src={react}
-              alt='react'
-              className='cont-ed-logo'
-            />
-          </Cell>
-
-          <Cell col={1}>
-            <img
-              src={nodejs}
-              alt='nodejs'
-              className='cont-ed-logo'
-            />
-          </Cell>
-          <Cell col={1}>
-            <img
-              src={mongodb}
-              alt='mongodb'
-              className='cont-ed-logo'
-            />
-          </Cell>
-
-          <Cell className='front-languages' col={5}>
-            <div>
-              <h2>Web Development Languages</h2>
-            </div>
-          </Cell>
-
-          <Cell className='source-control' col={8}>
-            <h2>Source Control</h2>
-          </Cell>
-
-          <Cell col={1}>
-            <img
-              src={github}
-              alt='github'
-              className='cont-ed-logo'
-            />
-          </Cell>
-
-          <Cell col={1}>
-            <img
-              src={bitbucket}
-              alt='bitbucket'
-              className='cont-ed-logo'
-            />
-          </Cell>
-
-          <Cell col={1}>
-            <img
-              src={gitkracken}
-              alt='gitkracken'
-              className='cont-ed-logo'
-            />
-          </Cell>
-
-          <Cell col={1}>
-            <img
-              src={vscode}
-              alt='vscode'
-              className='cont-ed-logo'
-            />
+          <Cell col={12}>
+            <div className='content'>{this.toggleCategories()}</div>
           </Cell>
         </Grid>
       </div>
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
 import {
-  Tabs,
-  Tab,
-  Grid,
-  Cell,
   Card,
   CardTitle,
   CardText,
@@ -14,180 +10,128 @@ import {
 } from 'react-mdl';
 
 class Projects extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { activeTab: 0 };
-  }
-
-  toggleCategories() {
-    if (this.state.activeTab === 0) {
-      return (
-        <div className='projects-grid'>
-          {/*--------------------------------------------------------------------------------------------------------- */}
-          <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-            <CardTitle
-              style={{
-                color: 'white',
-                height: '176px',
-                background:
-                  'url(https://aglowiditsolutions-6lohnuosd1nx.netdna-ssl.com/wp-content/uploads/2018/09/Hire-Mean-Stack-Developer.png) center / cover'
-              }}
-            />
-            <CardText style={{ fontWeight: 'bold', textAlign: 'center' }}>
-              Skyline/Skybest Irregular Plant Web Application
-            </CardText>
-            <CardText style={{ textAlign: 'center' }}>
-              This is a MEAN stack application built with AngularJS, NodeJS,
-              Express, and a MongoDB backend non-relational database.
-            </CardText>
-
-            <CardActions style={{ textAlign: 'center' }}>
-              <Button
-                colored
-                href='https://github.com/devansheets4/Irregular-Plant-App'
-                target='_blank'
-              >
-                GitHub
-              </Button>
-              <Button
-                colored
-                href='https://youtu.be/3hBDMfZwr80'
-                target='_blank'
-              >
-                View Desktop Demo
-              </Button>
-              <Button
-                colored
-                href='https://www.youtube.com/watch?v=V5nDs7EM7-0&t=3s'
-                target='_blank'
-              >
-                View Mobile Demo
-              </Button>
-            </CardActions>
-
-            <CardMenu style={{ color: 'fff' }}>
-              <IconButton name='share' />
-            </CardMenu>
-          </Card>
-          {/*--------------------------------------------------------------------------------------------------------- */}
-          <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-            <CardTitle
-              style={{
-                color: 'white',
-                height: '176px',
-                background:
-                  'url(https://www.codingdojo.com/blog/wp-content/uploads/html-css-js.png) center / cover'
-              }}
-            />
-            <CardText style={{ fontWeight: 'bold', textAlign: 'center' }}>
-              {' '}
-              Sheets Country Store{' '}
-            </CardText>
-            <CardText style={{ textAlign: 'center' }}>
-              My very first website ever built. Constructed using only barebones
-              HTML, CSS, JavaScript, and some Bootstrap. This was mainly a
-              project to build up my understanding of what goes into building
-              larger scale websites with JavaScript frameworks such as React and
-              Angular.
-            </CardText>
-
-            <CardActions style={{ textAlign: 'center' }}>
-              <Button
-                colored
-                href='https://github.com/devansheets4/Sheets-Country-Store'
-                target='_blank'
-              >
-                GitHub
-              </Button>
-              <Button
-                colored
-                href='http://sheetscountrystore.com/'
-                target='_blank'
-              >
-                Visit Site
-              </Button>
-            </CardActions>
-
-            <CardMenu style={{ color: 'fff' }}>
-              <IconButton name='share' />
-            </CardMenu>
-          </Card>
-          {/*--------------------------------------------------------------------------------------------------------- */}
-          <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-            <CardTitle
-              style={{
-                color: 'white',
-                height: '176px',
-                background:
-                  'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/800px-React-icon.svg.png) center / cover'
-              }}
-            />
-            <CardText style={{ fontWeight: 'bold', textAlign: 'center' }}>
-              Devan Sheets Web Portfolio
-            </CardText>
-            <CardText style={{ textAlign: 'center' }}>
-              The site you are currently sitting on if you are reading this.
-              Built using the React JavaScript framework. This is by far my
-              favorite framework to use. Check out the code in my GitHub link.
-            </CardText>
-
-            <CardActions style={{ textAlign: 'center' }}>
-              <Button
-                colored
-                href='https://github.com/devansheets4/Devan-Sheets-Portfolio'
-                target='_blank'
-              >
-                GitHub
-              </Button>
-            </CardActions>
-
-            <CardMenu style={{ color: 'fff' }}>
-              <IconButton name='share' />
-            </CardMenu>
-          </Card>
-          {/*--------------------------------------------------------------------------------------------------------- */}
-        </div>
-      );
-    } else if (this.state.activeTab === 1) {
-      return (
-        <div>
-          <h1> This is Angular </h1>
-        </div>
-      );
-    } else if (this.state.activeTab === 2) {
-      return (
-        <div>
-          <h1> This is VueJS </h1>
-        </div>
-      );
-    } else if (this.state.activeTab === 3) {
-      return (
-        <div>
-          <h1> This is MongoDB </h1>
-        </div>
-      );
-    }
-  }
-
   render() {
     return (
-      <div className='category-tabs'>
-        <Tabs
-          activeTab={this.state.activeTab}
-          onChange={tabId => this.setState({ activeTab: tabId })}
-          ripple
-        >
-          <Tab>React</Tab>
-          <Tab>Angular</Tab>
-          <Tab>VueJS</Tab>
-          <Tab>MongoDB</Tab>
-        </Tabs>
+      <div className='projects-grid'>
+        {/*--------------------------------------------------------------------------------------------------------- */}
+        <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+          <CardTitle
+            style={{
+              color: 'white',
+              height: '176px',
+              background:
+                'url(https://aglowiditsolutions-6lohnuosd1nx.netdna-ssl.com/wp-content/uploads/2018/09/Hire-Mean-Stack-Developer.png) center / cover'
+            }}
+          />
+          <CardText style={{ fontWeight: 'bold', textAlign: 'center' }}>
+            Skyline/Skybest Irregular Plant Web Application
+          </CardText>
+          <CardText style={{ textAlign: 'center' }}>
+            This is a MEAN stack application built with AngularJS, NodeJS,
+            Express, and a MongoDB backend non-relational database.
+          </CardText>
 
-        <Grid>
-          <Cell col={12}>
-            <div className='content'>{this.toggleCategories()}</div>
-          </Cell>
-        </Grid>
+          <CardActions style={{ textAlign: 'center' }}>
+            <Button
+              colored
+              href='https://github.com/devansheets4/Irregular-Plant-App'
+              target='_blank'
+            >
+              GitHub
+            </Button>
+            <Button colored href='https://youtu.be/3hBDMfZwr80' target='_blank'>
+              View Desktop Demo
+            </Button>
+            <Button
+              colored
+              href='https://www.youtube.com/watch?v=V5nDs7EM7-0&t=3s'
+              target='_blank'
+            >
+              View Mobile Demo
+            </Button>
+          </CardActions>
+
+          <CardMenu style={{ color: 'fff' }}>
+            <IconButton name='share' />
+          </CardMenu>
+        </Card>
+        {/*--------------------------------------------------------------------------------------------------------- */}
+        <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+          <CardTitle
+            style={{
+              color: 'white',
+              height: '176px',
+              background:
+                'url(https://www.codingdojo.com/blog/wp-content/uploads/html-css-js.png) center / cover'
+            }}
+          />
+          <CardText style={{ fontWeight: 'bold', textAlign: 'center' }}>
+            {' '}
+            Sheets Country Store{' '}
+          </CardText>
+          <CardText style={{ textAlign: 'center' }}>
+            My very first website ever built. Constructed using only barebones
+            HTML, CSS, JavaScript, and some Bootstrap. This was mainly a project
+            to build up my understanding of what goes into building larger scale
+            websites with JavaScript frameworks such as React and Angular.
+          </CardText>
+
+          <CardActions style={{ textAlign: 'center' }}>
+            <Button
+              colored
+              href='https://github.com/devansheets4/Sheets-Country-Store'
+              target='_blank'
+            >
+              GitHub
+            </Button>
+            <Button
+              colored
+              href='http://sheetscountrystore.com/'
+              target='_blank'
+            >
+              Visit Site
+            </Button>
+          </CardActions>
+
+          <CardMenu style={{ color: 'fff' }}>
+            <IconButton name='share' />
+          </CardMenu>
+        </Card>
+        {/*--------------------------------------------------------------------------------------------------------- */}
+        <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+          <CardTitle
+            style={{
+              color: 'white',
+              height: '176px',
+              background:
+                'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/800px-React-icon.svg.png) center / cover'
+            }}
+          />
+          <CardText style={{ fontWeight: 'bold', textAlign: 'center' }}>
+            Devan Sheets Web Portfolio
+          </CardText>
+          <CardText style={{ textAlign: 'center' }}>
+            The site you are currently sitting on if you are reading this. Built
+            using the React JavaScript framework. This is by far my favorite
+            framework to use. React is very simple and very easy to use. Check
+            out the code in my GitHub link.
+          </CardText>
+
+          <CardActions style={{ textAlign: 'center' }}>
+            <Button
+              colored
+              href='https://github.com/devansheets4/Devan-Sheets-Portfolio'
+              target='_blank'
+            >
+              GitHub
+            </Button>
+          </CardActions>
+
+          <CardMenu style={{ color: 'fff' }}>
+            <IconButton name='share' />
+          </CardMenu>
+        </Card>
+        {/*--------------------------------------------------------------------------------------------------------- */}
       </div>
     );
   }
